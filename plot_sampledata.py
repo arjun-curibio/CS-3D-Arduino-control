4a2aaeda6af84db226ef568f53a4c94217fbe54f
arjun-curibio <91759394+arjun-curibio@users.noreply.github.com>
2022-08-01 22:44:28 -0700

    .

--- plot_sampledata.py
+++ plot_sampledata.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('samplefile.txt', names=['t', 'motor','stretch'], header=0, index_col=0)
+df = pd.read_csv('samplefile.txt', names=['t', 'motor (steps)','stretch (%)'], header=0, index_col=0)
 df.index = df.index - df.index[0]
 # plt.ion()
 ax = df.plot()
@@ -9,7 +9,7 @@ ax = df.plot()
 plt.savefig('t vs both.png', format='png')
 
 plt.close('all')
-plt.plot(df['motor'], df['stretch'])
+plt.plot(df['motor (steps)'], df['stretch (%)'])
 plt.xlabel('Motor Position (steps)')
 plt.ylabel('Stretch %')
 plt.savefig('position vs stretch.png', format='png')
